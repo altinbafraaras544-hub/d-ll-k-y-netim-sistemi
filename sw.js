8d534fde6ce8ac4715b774070ce2eda80bfce21e
altinbafraaras544-hub <altinbafraaras544@gmail.com>
2026-09-07 17:48:20 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE='dolluk-v22';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/yonetim-logo.png',
+  './assets/super-lig-logo.png',
+  './assets/app-icon-192.png',
+  './assets/app-icon-512.png'
+];
+
+self.addEventListener('install', event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate', event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+
+  event.respondWith(
+    caches.match(req).then(cached=>{
+      const network=fetch(req).then(res=>{
+        if(res && res.ok && new URL(req.url).origin===self.location.origin){
+          const copy=res.clone();
+          caches.open(CACHE).then(c=>c.put(req,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached || network;
+    })
+  );
+});
